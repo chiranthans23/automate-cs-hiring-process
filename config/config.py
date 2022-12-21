d13cf84c1bcddfa6cd35b40922e6e565ef46e9c4
chiranthans23 <chiranthans23@gmail.com>
2022-12-21 03:18:01 -0700

    config and syn hired ppl

---
+++ config/config.py
@@ -0,0 +1,8 @@
+
+cfg = {
+    'students_sheet_name': "S23 Available Students for Hourly Positions",
+    'test_students_sheet_name': "Copy of S23 Available Students for Hourly Positions",
+    'hired_students_sheet_name': "Spring 23 Grader and Course Manager Hire Request Form (Responses)"
+
+
+}
---
+++ sync_hired_ppl.py
@@ -0,0 +1,45 @@
+"""
+This script syncs people who are hired (from the hired people sheet) 
+to the available students sheet
+
+"""
+
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from credentials.read_creds import get_creds
+from config.config import cfg 
+from sort_hiring import sort_sheet_hired
+
+
+def sync_hired():
+    creds = get_creds()
+    file = gspread.authorize(creds) 
+
+
+    sheet = file.open(cfg['hired_students_sheet_name'])
+    sheet = sheet.get_worksheet(0)
+    list_of_lists = sheet.get_all_values()[2:]
+
+    emails = []
+
+    for row in list_of_lists:
+        emails.append(row[4].strip().lower())
+
+    sheet = file.open(cfg['students_sheet_name'])
+    sheet = sheet.get_worksheet(0)
+    list_of_lists = sheet.get_all_values()[2:]
+
+    for i, row in enumerate(list_of_lists):
+        if row[1].lower()!='hired' and row[2].strip().lower() in emails:
+            sheet.update_cell(i+3,2, 'Hired')
+
+    sort_sheet_hired()
+
+
+if __name__ == '__main__':
+    sync_hired()
+
+
+
+
